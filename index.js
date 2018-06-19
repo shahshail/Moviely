46ec886ca470deaa16be6b5f8947db12ddd48475
Shail Shah <shahshail9898@gmail.com>
2018-06-19 00:05:55 -0400

    Handling PUT Request

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.get('/api/genres', (req, res) => {
   });
   
 
+  app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
+    const { error } = validateGenre(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    genre.name = req.body.name; 
+    res.send(genre);
+  });
+  
 
 // PORT 
 const port = process.env.PORT || 3000;
