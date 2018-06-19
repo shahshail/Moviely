e273e1f6681b24eb539e06aabfe3efc492ed4181
Shail Shah <shahshail9898@gmail.com>
2018-06-19 00:07:22 -0400

    Handling Delete Request

--- index.js
+++ index.js
@@ -35,7 +35,30 @@ app.get('/api/genres', (req, res) => {
     genre.name = req.body.name; 
     res.send(genre);
   });
+  app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+  
+    res.send(genre);
+  });
+  
+  app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    res.send(genre);
+  });
+  
+  function validateGenre(genre) {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+  
+    return Joi.validate(genre, schema);
+  }
+    
 
 // PORT 
 const port = process.env.PORT || 3000;
