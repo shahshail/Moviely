8bc13418778a20456de6d7da84fe67651caae9b6
Shail Shah <shahshail9898@gmail.com>
2018-06-29 00:13:39 -0400

    Config

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-import Joi from 'joi';
-import config from 'config';
-import morgan from 'morgan';
-import helmet from 'helmet'
-import genres from './routes/genres';
-import express, { json } from 'express';
+const Joi = require('joi');
+const config = require('config');
+const helmet = require('helmet')
+const morgan = require('morgan');
+const genres = require('./routes/genres');
+const express = require('express');
 const app = express();
 
-console.log(`app : ${app.get('env')}`)
-
-app.use(morgan())
-app.use(helmet())
-app.use(json());
+app.use(helmet)
+app.use(express.json());
 app.use('/api/genres', genres);
+
+
+if(app.get('env') == 'development')// To see the current working environment
+{
+    app.use(morgan('tiny'));
+    console.log(`Current environment is: ${app.get('env')}`)
+    console.log("Morgan Enabled...")
+}
 
 const port = process.env.PORT || 9898;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
