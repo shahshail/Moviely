c2a08c4849781db58423d9f657db39b26215bcc6
Shail Shah <shahshail9898@gmail.com>
2018-06-29 00:06:15 -0400

    Cleanup | ES6 support

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const Joi = require('joi');
-const genres = require('./routes/genres');
-const express = require('express');
+import Joi from 'joi';
+import config from 'config';
+import morgan from 'morgan';
+import helmet from 'helmet'
+import genres from './routes/genres';
+import express, { json } from 'express';
 const app = express();
 
-app.use(express.json());
+console.log(`app : ${app.get('env')}`)
+
+app.use(morgan())
+app.use(helmet())
+app.use(json());
 app.use('/api/genres', genres);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9898;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
