183d5bc9cf4b8842f388a0c537b0a025a5d17654
Shail Shah <shahshail9898@gmail.com>
2018-07-12 00:36:47 -0400

    Refactor and Cleanup Customer Model

--- routes/customers.js
+++ routes/customers.js
@@ -1,26 +1,8 @@
-const Joi = require('joi');
+const {Customer, validateCustomer} = require('..//models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-    type: Boolean,
-    default: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -73,14 +55,5 @@ router.get('/:id', async (req, res) => {
   res.send(customer);
 });
 
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean()
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 module.exports = router; 
